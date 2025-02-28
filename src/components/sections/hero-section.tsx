d21a888f9801426b25ff37b70ba7adc305c8a3d1
June Rhomel <junemandigma@gmail.com>
2025-03-01 02:43:06 +0800

    compress iame

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -149,7 +149,7 @@ export function HeroSection() {
         />
       </div>
 
-      <div className="container px-4 md:px-6 m-auto relative m-auto">
+      <div className="container px-4 md:px-6 m-auto relative ">
         <motion.div
           initial="hidden"
           animate={controls}
